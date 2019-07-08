cf5e3c0360243e0398f98af82c55c324c92527ff
twinklekhandelwal <45447367+twinklekhandelwal@users.noreply.github.com>
2019-07-08 15:36:39 +0530

    Update translet.py

--- ng_exceptions/translet.py
+++ ng_exceptions/translet.py
@@ -3,16 +3,9 @@ def translets(formatted):
     a="ng_exceptions/"+"translet.json"
     with open(a, 'r') as f:
         error_list = json.load(f)
-
-    
-    
-    
     for i in error_list:
-        
         english_error=i["from"]
         hindi_error=i["to"]
-        
-
         if english_error in formatted:
             formatted+=hindi_error
             return formatted    
